3ff703a83720e73466881c33b8a4b0b639d76ef1
crileyy <criley0007@gmail.com>
2021-02-05 00:02:12 -0500

    Previous button for clicy activity

--- app/src/main/java/edu/neu/madcourse/numad21sp_colinriley/ClickyActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21sp_colinriley/ClickyActivity.java
@@ -2,7 +2,9 @@ package edu.neu.madcourse.numad21sp_colinriley;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ClickyActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ClickyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky);
+    }
+
+    public void onClickPrevious(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
