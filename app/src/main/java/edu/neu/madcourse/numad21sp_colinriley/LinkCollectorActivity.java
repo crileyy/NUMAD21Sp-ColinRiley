3350b5ab27d62366d2aab1051dd5138ee029862b
crileyy <criley0007@gmail.com>
2021-02-13 15:16:39 -0500

    Added some link collector activity code and navigation

--- app/src/main/java/edu/neu/madcourse/numad21sp_colinriley/LinkCollectorActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21sp_colinriley/LinkCollectorActivity.java
@@ -1,14 +1,59 @@
 package edu.neu.madcourse.numad21sp_colinriley;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class LinkCollectorActivity extends AppCompatActivity {
+
+    private List<LinkItemCard> itemList = new ArrayList<>();
+
+    private RecyclerView recyclerView;
+    private LinkReviewAdapter rviewAdapter;
+    private RecyclerView.LayoutManager rLayoutManger;
+    private FloatingActionButton addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_collector);
+
+        addButton = findViewById(R.id.addLinkButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pos = 0;
+                addItem(pos);
+            }
+        });
+    }
+
+    private void init(Bundle savedInstanceState) {
+        initialItemData(savedInstanceState);
+        createRecyclerView();
+    }
+
+    private void initialItemData(Bundle savedInstanceState) {
+
+    }
+
+    private void createRecyclerView() {
+
+    }
+
+    private void addItem(int position) {
+        itemList.add(position, new LinkItemCard("add name", "add url"));
+        Toast.makeText(LinkCollectorActivity.this, "Add an item", Toast.LENGTH_SHORT).show();
+
+        rviewAdapter.notifyItemInserted(position);
     }
 }
